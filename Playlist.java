b356196b7118b3064e8b8f8b2314afbc3b0b234d
Jerry Fu <jfu25@students.polytechnic.org>
2024-02-06 20:02:19 -0800

    Finished playlist class

--- Playlist.java
+++ Playlist.java
@@ -9,6 +9,7 @@ public class Playlist {
      * Fields-- This will likely just need to be the ArrayList of Songs. Reference our previous problems
      * (CarDealership, Zoo) for structure on how this will look
      */
+    ArrayList<Song> playlist;
 
 
 
@@ -17,7 +18,9 @@ public class Playlist {
       * Constructor-- this doesn't need any parameters. You should just initialize the ArrayList and
       * then use additional methods to add Songs in one-by-one
       */
-
+    public Playlist() {
+        playlist = new ArrayList<Song>();
+    }
 
       
 
@@ -31,4 +34,40 @@ public class Playlist {
        * Determining the total duration of all songs
        * Removing all unliked songs from the playlist (careful with this one!)
        */
+      public void likeSong(boolean status, int index) {
+        playlist.get(index).setStatus(true);
+      }
+      public void addSong(boolean status, int duration, String name, String artist) {
+        playlist.add(new Song(duration, name, artist));
+      }
+      public void removeSong(int index) {
+        playlist.remove(index);
+      }
+      public void printSong() {
+        for (int i = 0; i < playlist.size(); i++) {
+            System.out.println(playlist.get(i));
+        }
+      }
+      public void likedSong() {
+        for (int i = 0; i < playlist.size(); i++) {
+            if (playlist.get(i).getStatus() == true) {
+                System.out.println(playlist.get(i));
+            }
+        }
+      }
+      public int totalDuration() {
+        int sum = 0;
+        for (int i = 0; i < playlist.size(); i++) {
+            sum += playlist.get(i).getDuration();
+        }
+        return sum;
+      }
+      public void removeUnliked() {
+        for (int i = 0; i < playlist.size(); i++) {
+            if (playlist.get(i).getStatus() == false) {
+                playlist.remove(i);
+                i--;
+            }
+        }
+      }
 }
