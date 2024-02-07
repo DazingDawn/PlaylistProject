885ecc675577759fa29a52efb85a45b3fc85ab9a
Jerry Fu <jfu25@students.polytechnic.org>
2024-02-06 19:36:20 -0800

    Finished song class

--- Song.java
+++ Song.java
@@ -1,11 +1,14 @@
 /**
  * File for a Song class to be used in the Playlist Project
- * @author
- * @version
+ * @author Jerry Fu
+ * @version 2/6/24
  */
 public class Song {
     //Fields-- what information do we want each Song to store?
-
+    private boolean status;
+    private int duration;
+    private String name;
+    private String artist;
 
 
 
@@ -14,7 +17,12 @@ public class Song {
      * How will you handle 'liked' songs? It makes sense for a Song not be 'liked' by default
      */
 
-
+    public Song(int duration, String name, String artist) {
+        this.status = false;
+        this.duration = duration;
+        this.name = name;
+        this.artist = artist;
+    }
 
 
 
@@ -25,6 +33,27 @@ public class Song {
       * song easily as well!
       * What kind of mutator (setter) methods will you need?
       */
-
-
+    public boolean getStatus() {
+        return status;
+    }
+    public int getDuration() {
+        return duration;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getArtist() {
+        return artist;
+    }
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    public String toString() {
+        if(status == false) {
+            return "\"" + name + "\"" + " by " + artist + " (" + duration + ") ";
+        }
+        else {
+            return "\"" + name + "\"" + " by " + artist + " (" + duration + ") " + " - liked";
+        }
+    }
 }
